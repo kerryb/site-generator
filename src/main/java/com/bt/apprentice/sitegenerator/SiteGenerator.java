6b0adec76e6176cafe4801f644c66102d31bbddb
Kerry Buckley <kerryjbuckley@gmail.com>
2013-07-22 20:14:20 +0100

    List files in site directory

--- src/main/java/com/bt/apprentice/sitegenerator/SiteGenerator.java
+++ src/main/java/com/bt/apprentice/sitegenerator/SiteGenerator.java
@@ -1,6 +1,12 @@
 package com.bt.apprentice.sitegenerator;
 
+import java.io.File;
+
 public class SiteGenerator {
   public static void main(String[] args) {
+    File[] files = new File("site").listFiles();
+    for (File file : files) {
+      System.out.println(file.getAbsolutePath());
+    }
   }
 }
