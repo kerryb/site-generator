38ce92622e1e853984c5c61ac9edada497f0a2b5
Kerry Buckley <kerryjbuckley@gmail.com>
2013-07-22 21:13:45 +0100

    Read and print out file contents

--- src/main/java/com/bt/apprentice/sitegenerator/SiteGenerator.java
+++ src/main/java/com/bt/apprentice/sitegenerator/SiteGenerator.java
@@ -1,12 +1,17 @@
 package com.bt.apprentice.sitegenerator;
 
 import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 
 public class SiteGenerator {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     File[] files = new File("site").listFiles();
     for (File file : files) {
-      System.out.println(file.getAbsolutePath());
+      String path = file.getAbsolutePath();
+      String content = FileUtils.readFileToString(file, "UTF-8");
+      System.out.println(content);
     }
   }
 }
