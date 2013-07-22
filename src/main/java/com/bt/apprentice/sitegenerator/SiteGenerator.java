b506d7c9e41a51d787b7f01bd64c84bf9dd66e3b
Kerry Buckley <kerryjbuckley@gmail.com>
2013-07-22 22:21:29 +0100

    Move path variable to where it's used

--- src/main/java/com/bt/apprentice/sitegenerator/SiteGenerator.java
+++ src/main/java/com/bt/apprentice/sitegenerator/SiteGenerator.java
@@ -10,10 +10,10 @@ public class SiteGenerator {
   public static void main(String[] args) throws IOException {
     File[] files = new File("site").listFiles();
     for (File file : files) {
-      String path = file.getAbsolutePath();
       String content = FileUtils.readFileToString(file, "UTF-8");
       PegDownProcessor processor = new PegDownProcessor();
       String compiled = processor.markdownToHtml(content);
+      String path = file.getAbsolutePath();
       String newPath = path.replaceAll(".markdown$", ".html");
       File newFile = new File(newPath);
       FileUtils.writeStringToFile(newFile, compiled);
