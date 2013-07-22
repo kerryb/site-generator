d1c604b8abe78d9dd217898db71e996f923ab6f4
Kerry Buckley <kerryjbuckley@gmail.com>
2013-07-22 21:41:24 +0100

    Extract getFiles and writeCompiledFile

--- src/main/java/com/bt/apprentice/sitegenerator/SiteGenerator.java
+++ src/main/java/com/bt/apprentice/sitegenerator/SiteGenerator.java
@@ -8,15 +8,23 @@ import org.pegdown.PegDownProcessor;
 
 public class SiteGenerator {
   public static void main(String[] args) throws IOException {
-    File[] files = new File("site").listFiles();
+    File[] files = listFiles();
     for (File file : files) {
-      String path = file.getAbsolutePath();
-      String content = FileUtils.readFileToString(file, "UTF-8");
-      PegDownProcessor processor = new PegDownProcessor();
-      String compiled = processor.markdownToHtml(content);
-      String newPath = path.replaceAll(".markdown$", ".html");
-      File newFile = new File(newPath);
-      FileUtils.writeStringToFile(newFile, compiled);
+      writeCompiledFile(file);
     }
+  }
+
+  private static File[] listFiles() {
+    return new File("site").listFiles();
+  }
+
+  private static void writeCompiledFile(File file) throws IOException {
+    String path = file.getAbsolutePath();
+    String content = FileUtils.readFileToString(file, "UTF-8");
+    PegDownProcessor processor = new PegDownProcessor();
+    String compiled = processor.markdownToHtml(content);
+    String newPath = path.replaceAll(".markdown$", ".html");
+    File newFile = new File(newPath);
+    FileUtils.writeStringToFile(newFile, compiled);
   }
 }
